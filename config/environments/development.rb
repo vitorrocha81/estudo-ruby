ab7847a9c3f71bb94bc8681472df2656ca7fa448
VITOR DESKTOP <nelsonvitorsrocha@gmail.com>
2017-03-02 17:09:24 -0300

    seeds and foreman fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #foreman list activities on console
+  $stdout.sync = true
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Criando as CATEORIRIAS"
+
+categories = ["Animais",
+			 				"Esportes",
+			 				"Casa",
+			 				"Eletrônicos",
+			 				"Celulares",
+			 				"Música",
+			 				"Moda e beleza",
+			 				"Veículos",
+			 				"Imóveis",
+			 				"Empregos",
+			 				"Serviços",
+			 				"Outros",
+			 					]
+
+categories.each do |category|
+	Category.find_or_create_by(description: category)
+end
+
+puts "Categorias cadastradas com sucesso"
