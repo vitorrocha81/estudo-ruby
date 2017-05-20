a2cd66d5c3a4cde258294bf6171c8cacffeb8440
VITOR <vitor.rocha81@bol.com.br>
2017-05-20 20:25:55 -0300

    ads controller actions

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -5,4 +5,28 @@ class Site::Profile::AdsController < Site::ProfileController
   def index
   	@ads = Ad.where(member_id: current_member)
   end
+
+  def show
+  	@ad = Ad.find(params[:id])
+  end
+
+  def new
+  	@ad = Ad.new
+  end
+
+  def create
+  	@ad = Ad.create(ad_params)
+  	if @ad.save
+  		redirect_to  site_profile_ads_path  , notice: "Criado com sucesso"
+  	else
+  		render :new
+  	end
+  end
+
+  private
+
+  def ad_params
+  	 params.require(:ad).permit(:price, :category_id, :description, :title, :picture)
+  end
+
 end
